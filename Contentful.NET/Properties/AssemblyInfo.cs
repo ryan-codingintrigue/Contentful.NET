00c5f8f9cd0f4b5f3b38d392b5395d4197a681e6
ryan-codingintrigue <ryan@codingintrigue.co.uk>
2015-02-26 13:05:54 +0000

    - Migrated main DLL to a PCL-friendly project - Bumped NuGet dependencies to most recent version

--- Contentful.NET/Properties/AssemblyInfo.cs
+++ Contentful.NET/Properties/AssemblyInfo.cs
@@ -19,9 +19,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a5dcde5d-76e4-4eda-b5ca-c00d6963994c")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
